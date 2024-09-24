b894063242727c1e0028ae30ed447eb7b9169f27
PontusEkstrom02 <113009598+PontusEkstrom02@users.noreply.github.com>
2024-09-25 00:26:02 +0200

    continued working on header, about done

--- frontend/src/app/header/nav/nav.component.ts
+++ frontend/src/app/header/nav/nav.component.ts
@@ -1,12 +1,30 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.scss'
 })
 export class NavComponent {
-
+  list = [
+    {
+      title: 'konakta',
+      dropdown: ['kontakta mig', 'kontakta dig', 'kontakta anka', 'kontakta annan anka']
+    },
+    {
+      title: 'spela',
+      dropdown: ['spela mig', 'spela dig', 'spela anka', 'spela annan anka']
+    },
+    {
+      title: 'handla',
+      dropdown: ['handla mig', 'handla dig', 'handla anka', 'handla annan anka']
+    },
+    {
+      title: 'kasta',
+      dropdown: ['kasta mig', 'kasta dig', 'kasta anka', 'kasta annan anka']
+    }
+  ]
 }
