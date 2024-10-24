6ee4a8c0de0e022c6324fde3a55e6b97ee7485f5
Tussee098 <Tusse098@hotmail.com>
2024-10-24 22:48:53 +0200

    ¨fixes for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,36 +1,32 @@
-import app from './app.js'; // Import the app
-import mongoose from 'mongoose';
 import express from 'express';
-import path from 'path'; // Import path module
+import mongoose from 'mongoose';
+import path from 'path';
 import config from './config.js';
-import { fileURLToPath } from 'url'; // Import fileURLToPath
-import { dirname } from 'path'; // Import dirname
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
-// Get the current directory name
-const __filename = fileURLToPath(import.meta.url); // Get the current file name
-const __dirname = dirname(__filename); // Get the directory name
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const app = express();
 
 // Connect to MongoDB
 const DBUrl = config.dbUrl;
-const port = config.port; // Changed to match the config variable
+const port = config.port;
 
 mongoose.connect(DBUrl)
   .then(() => {
     console.log('MongoDB connected');
-
+    
     app.use(express.json()); // Parse incoming requests
 
     // Serve static files from Angular app
-    const frontendPath = path.join(__dirname, '../../frontend/dist/'); // Construct the path to your frontend files
-    app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname, '/client/dist', 'index.html'))
-    })
+    const frontendPath = path.join(__dirname, '../../frontend/dist/frontend'); // Path to your Angular build
     app.use(express.static(frontendPath));
 
-    // Handle all GET requests to serve the Angular app
-    /*app.get('*', (req, res) => {
-      res.sendFile(path.join(frontendPath, 'index.html')); // Serve index.html for all routes
-    });*/
+    // Serve index.html for any other requests
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(frontendPath, 'index.html'));
+    });
 
     app.listen(port, () => {
       console.log(`Backend is running on http://localhost:${port}`);
