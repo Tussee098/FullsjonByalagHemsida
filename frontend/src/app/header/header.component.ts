f1bf77676782d82a1d55d5233f5025b994d98f0c
Tussee098 <Tusse098@hotmail.com>
2024-11-03 14:11:59 +0100

    header styling (not working)

--- frontend/src/app/header/header.component.ts
+++ frontend/src/app/header/header.component.ts
@@ -1,7 +1,7 @@
 import { NgIf } from '@angular/common';
 import { AuthService } from './../services/authService';
 import { NavComponent } from './nav/nav.component';
-import { Component } from '@angular/core';
+import { Component, ElementRef, AfterViewInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { GardenComponent } from '../illustrations/garden/garden.component';
 
@@ -18,7 +18,30 @@ export class HeaderComponent {
   loggedIn: boolean = false;
   loading: boolean = true; // Add loading flag
 
+  @ViewChild('logoContainer') logoContainer!: ElementRef;
+  @ViewChild('app-nav') appNav!: ElementRef;
+  logoVisible: boolean = true;
+
   constructor(private authService: AuthService, private router: Router){};
+
+  ngAfterViewInit() {
+    if (this.logoContainer && this.appNav) {
+      const observer = new IntersectionObserver(
+        (entries) => {
+          entries.forEach((entry) => {
+            this.logoVisible = !entry.isIntersecting;
+          });
+        },
+        {
+          root: null, // Use the viewport as the root
+          threshold: 0.5, // Adjust to trigger halfway through overlap
+        }
+      );
+
+      // Start observing the navigation element
+      observer.observe(this.appNav.nativeElement);
+    }
+  }
 
   async ngOnInit() {
     this.loggedIn = await this.authService.isLoggedIn(); // Use await to handle the promise
